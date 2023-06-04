2c42e51c4f290b5e6caa1302bff6295637dbefe1
Absurdnoobman <jajasamag@gmail.com>
2023-06-04 18:49:00 +0700

    Error

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,71 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Modulo()
-      ),
+      home: Scaffold(body: Modulo()),
     );
   }
 }
 
 class Modulo extends StatefulWidget {
   const Modulo({super.key});
-
   @override
   State<Modulo> createState() => _ModuloState();
 }
 
 class _ModuloState extends State<Modulo> {
+
+  // void calculate() {
+    
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return const Row(
+
+    int a;
+    int b;
+
+    // final myController = TextEditingController();
+
+    // @override
+    // void initState() {
+    //   super.initState();
+    //   myController.addListener(calculate) ;
+    // }
+
+    // @override
+    // void dispose() {
+    //   myController.dispose();
+    //   super.dispose();
+    // }
+
+    return Center(
+        child: Column(
       children: [
-        Text(
-          "หารเอาเศษ", 
-          textAlign: TextAlign.center,
+        const Padding(
+          padding: EdgeInsets.only(top: 100.0, bottom: 50.0),
+          child: Expanded(
+            child: Text(
+              "หารเอาเศษ",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+          )
         ),
-        
+        Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(5.5),
+              child: TextField(
+                onChanged: (value) {
+                  a = int.parse(value);
+                },
+              ),
+            )
+          ],
+        )
       ],
-    );
+    ));
   }
 }
